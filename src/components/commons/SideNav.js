cfcec1c092cc7309a802e84390ed50af15b4f51e
iamkrati22 <iamkrati22@gmail.com>
2022-12-23 15:45:11 +0530

    Initial Commit

---
+++ src/components/commons/SideNav.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../stylesheets/SideNav.css'
+class SideNav extends React.Component {
+    render() {
+        return (
+            <section id="SideNav">
+                <h2 id="site-name"><a href="/">SideNav</a></h2>
+            </section>
+        );
+    }
+}
+export default SideNav;
